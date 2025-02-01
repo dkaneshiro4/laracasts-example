d33afab59226824db4eb8afd733b8b893ccb5e46
dkaneshi <dkaneshi@gmail.com>
2025-02-01 04:48:48 -1000

    Revamp jobs list layout with improved styling.
    Replaced the unordered list with a vertically spaced div structure for better readability. Enhanced job item styling with borders, padding, and added employer name display for a more polished user interface.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,15 @@
         Jobs Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-600 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-300">
+                <div class="text-blue-600 font-bold">{{ $job->employer->name }}</div>
+
+                <div>
                     <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
