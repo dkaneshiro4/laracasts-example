8ac546c057c7a111e627c84a3f1d63c82d731177
dkaneshi <dkaneshi@gmail.com>
2025-02-01 09:00:14 -1000

    Add CRUD functionality for job listings
    Implemented create, read, update, and delete operations for job listings, including form views for create/edit and routes for all actions. Updated blade templates for consistency and added pagination to the job index view.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,13 +5,17 @@
 
     <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-300">
+            <a href="/jobs/{{ $job->id }}" class="block px-4 py-6 border border-gray-300">
                 <div class="text-blue-600 font-bold">{{ $job->employer->name }}</div>
 
                 <div>
-                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
+                    <strong>{{ $job->title }}</strong>: Pays {{ $job->salary }} per year
                 </div>
             </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,72 @@ Route::get('/', function () {
      return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+
+    return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+// Show
 Route::get('/jobs/{id}', function($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+// Store
+Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 3
+    ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    // authorize (on hold...)
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/'. $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function($id) {
+    // authorize
+
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
