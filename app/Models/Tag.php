ef41216a38ef8bd75756dcf7980f3bddd222f8a5
dkaneshi <dkaneshi@gmail.com>
2025-02-01 04:48:32 -1000

    Add 'name' to Tag model's fillable properties
    This allows mass assignment of the 'name' attribute, enabling easier updates and creation of Tag instances. It ensures alignment with best practices for Eloquent models in Laravel.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -11,6 +11,8 @@ class Tag extends Model
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
 
+    protected $fillable = ['name'];
+
     public function jobs(): BelongsToMany
     {
         return $this->belongsToMany(Job::class, relatedPivotKey: 'job_listing_id');
