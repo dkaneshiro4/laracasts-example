bd9321a8e264afa095d92c118e157bb3fac43d2b
dkaneshi <dkaneshi@gmail.com>
2025-02-01 06:48:22 -1000

    Add JobSeeder and integrate it into DatabaseSeeder
    Introduced a new JobSeeder to seed 200 job records using factories. Updated DatabaseSeeder to call JobSeeder and adjusted user seeding for better test data consistency.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+//         User::factory(10)->create();
 
-//        User::factory()->create([
-//            'first_name' => 'Test',
-//            'last_name' => 'User',
-//            'email' => 'test@example.com',
-//        ]);
-        Job::factory(10)->create();
+        User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test@example.com',
+        ]);
 
+        $this->call(JobSeeder::class);
     }
 }
