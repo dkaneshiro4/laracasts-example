8a46ee16b150365572c355ee987cea79c27cc84b
dkaneshi <dkaneshi@gmail.com>
2025-02-01 22:05:18 -1000

    Implement user authentication with login, logout, and validation.
    Updated `SessionController` and `RegisteredUserController` to handle authentication, including login and logout functionality, form validation, and user redirection. Modified views and routes to support authentication, added session token regeneration, and updated the `User` model to handle first and last names.

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,7 +16,22 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'], // 'confirmed' can be used with email too. Looks for email_confirmation field
+            'password' => ['required', Password::min(6), 'confirmed'], // 'confirmed' looks for a password_confirmation field in the request
+        ]);
+
+        // create the user
+        $user = User::create($validatedAttributes);
+
+        // log in
+        Auth::login($user);
+
+        // redirect somewhere
+        return redirect('/jobs');
     }
 
 }
