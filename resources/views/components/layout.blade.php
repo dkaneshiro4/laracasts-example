46d65ebc14d136d7e57dac078c24dd9df87e1202
dkaneshi <dkaneshi@gmail.com>
2025-02-02 12:24:56 -1000

    Remove unused locale attribute and comment out Vite import
    The `lang` attribute on the `<html>` tag was removed because it was redundant. The Vite import was commented out as it is currently not being used and may be re-enabled later if needed. These changes help streamline the layout component.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<html class="h-full bg-gray-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Home</title>
+{{--    @vite(['resources/js/app.js', 'resources/css/app.css']);--}}
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
